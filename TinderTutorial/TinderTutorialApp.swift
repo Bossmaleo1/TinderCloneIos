e69aeb6f5407a18bc711d04eaf9ebd05f01224bd
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-05-14 13:41:31 +0200

    Initial Commit

---
+++ TinderTutorial/TinderTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialApp.swift
+//  TinderTutorial
+//
+//  Created by Sidney MALEO on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
